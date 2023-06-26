58f64f0b22c5cf31e63c1b2ca4cd951f31690a6b
Abelgukwe <abelgukwe@gmail.com>
2023-06-26 14:46:06 +0200

    ch2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,35 @@ class Homepage extends StatelessWidget {
         title: Text("Awesome App"),
       ),
       body: Container(
-        child: Text("Hi Flutter"),
+        color: Colors.teal,
+        height: 300,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+              alignment: Alignment.center,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            ),
+          ],
+        ),
       ),
     );
   }
