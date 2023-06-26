f82a9501c0674bd621ea1bc00cace839d0bf66b1
Abelgukwe <abelgukwe@gmail.com>
2023-06-26 13:14:58 +0200

    chapter 1

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Homepage(),
+    theme: ThemeData(
+      primarySwatch: Colors.purple,
+    ),
+  ));
+}
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Awesome App"),
+      ),
+      body: Container(
+        child: Text("Hi Flutter"),
+      ),
+    );
+  }
+}
